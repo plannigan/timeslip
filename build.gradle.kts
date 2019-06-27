3ef991e7dbaa77d29e520b5691bf33cf676e4128
Patrick Lannigan <p.lannigan@gmail.com>
2019-06-26 20:54:49 -0400

    Add support for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,18 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.3.31"
     jacoco
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
     id("org.jlleitschuh.gradle.ktlint") version "8.1.0"
     id("org.jlleitschuh.gradle.ktlint-idea") version "8.1.0"
 }
 
 group = "com.hypercubetools"
-version = "0.1-SNAPSHOT"
+version = "0.1-alpha"
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -24,3 +27,42 @@ tasks.withType<KotlinCompile> {
 
 testWithJunit()
 coverageWithJacoco()
+
+bintray {
+    user = System.getenv("BINTRAY_USER") ?: project.properties["bintray.user"]?.toString()
+    key = System.getenv("BINTRAY_KEY")
+    publish = true
+
+    setPublications("lib")
+
+    with(pkg) {
+        repo = project.group.toString()
+        name = project.name
+        setLicenses("MIT")
+        with(version) {
+            name = project.version.toString()
+        }
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("lib") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("name", project.name)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", "MIT")
+                        appendNode("url", "https://opensource.org/licenses/MIT")
+                        appendNode("distribution", "repo")
+                    }
+                }
+            }
+        }
+    }
+}
